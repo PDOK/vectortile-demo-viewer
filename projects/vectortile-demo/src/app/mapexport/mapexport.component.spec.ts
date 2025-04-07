98abd9f1b136d29126eb3670f9855ef503892374
john.schaap@kadaster.nl <John.Schaap@Kadaster.nl>
2025-04-07 15:36:47 +0200

    update urls

--- projects/vectortile-demo/src/app/mapexport/mapexport.component.spec.ts
+++ projects/vectortile-demo/src/app/mapexport/mapexport.component.spec.ts
@@ -1,25 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { MapexportComponent } from './mapexport.component';
 
 describe('MapexportComponent', () => {
-  let component: MapexportComponent;
-  let fixture: ComponentFixture<MapexportComponent>;
+  let component: MapexportComponent
+  let fixture: ComponentFixture<MapexportComponent>
 
+  // Set up the testing environment before each test
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapexportComponent ]
+     imports: [MapexportComponent]
     })
-    .compileComponents();
-  });
+      .compileComponents()
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MapexportComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    fixture = TestBed.createComponent(MapexportComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+  it('should create the MapexportComponent instance', () => {
+    // fixture.detectChanges() moved to beforeEach block
+  })
+})
